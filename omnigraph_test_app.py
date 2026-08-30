2be4dcc327c9fcd91875f3892623406a3b91b4d4
tzegoat9 <tzegoat9@gmail.com>
2026-08-30 17:31:49 +0530

    refactor: remove update_inventory ts=1788091302 (#27)

--- omnigraph_test_app.py
+++ omnigraph_test_app.py
@@ -1,13 +1,7 @@
 
-# OmniGraph e2e test ts=1788090624
+# OmniGraph deletion test ts=1788091302
 class Order:
     __tablename__ = "orders"
 
-class Inventory:
-    __tablename__ = "inventory"
-
 def process_order(order_id):
     Order.objects.filter(id=order_id).update(status="shipped")
-
-def update_inventory(product_id):
-    Inventory.objects.filter(product_id=product_id).update(qty=0)
