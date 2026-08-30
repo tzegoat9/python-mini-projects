7abe7cb9eabd314f3e5cf39034f49da59a169944
tzegoat9 <tzegoat9@gmail.com>
2026-08-30 17:41:53 +0530

    refactor: remove everything ts=1788091911

--- omnigraph_test_app.py
+++ omnigraph_test_app.py
@@ -1,7 +1,3 @@
 
-# OmniGraph deletion test ts=1788091302
-class Order:
-    __tablename__ = "orders"
-
-def process_order(order_id):
-    Order.objects.filter(id=order_id).update(status="shipped")
+# OmniGraph deletion test ts=1788091911
+# Everything is deleted
